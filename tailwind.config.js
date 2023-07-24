0db4452bf429621797e2cd4a22f99211e74dba33
Andi Lafito <lafitoandi27@gmail.com>
2023-07-24 11:16:02 +0700

    testing

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['/pages/*.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {},
+    mytheme: {
+      primary: '#570df8',
+      secondary: '#f000b8',
+      accent: '#1dcdbc',
+      neutral: '#2b3440',
+      'base-100': '#ffffff',
+      info: '#3abff8',
+      success: '#36d399',
+      warning: '#fbbd23',
+      error: '#f87272',
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    require('flowbite/plugin')],
+}
